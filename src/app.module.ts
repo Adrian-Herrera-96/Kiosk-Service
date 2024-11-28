f18c1c5e945a23425d7ead2c19a24cd339527947
gongor7 <crisgongor7@gmail.com>
2024-11-28 13:59:36 -0400

    Importación de modululo de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { KioskModule } from './kiosk/kiosk.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CommonModule, KioskModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, CommonModule, KioskModule],
   controllers: [],
   providers: [],
 })
