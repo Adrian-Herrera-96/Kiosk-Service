b732cf64267fd5d129251cb529c5c69896f38704
gongor7 <crisgongor7@gmail.com>
2024-11-28 14:18:11 -0400

    Creación de entidad para guardar la información de la autenticación

---
+++ src/kiosk/entities/kiosk-authentication-data.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ schema: 'kiosk', name: 'kiosk_authentication_data' })
+export class KioskAuthenticationData {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  person_id: number;
+
+  @Column()
+  identity_card: string;
+
+  @Column()
+  left_text: string;
+
+  @Column()
+  right_text: string;
+
+  @Column()
+  ocr_state: boolean;
+
+  @Column()
+  facial_recognition: boolean;
+
+  @Column()
+  recognized_text_captured: string;
+}
