4de4cf61ac623934af960028b1d58080c2fb7b24
Adrian-Herrera-96 <adrirha@gmail.com>
2024-12-19 11:37:07 -0400

    Refactorización de ftp client para aceptar multiples consultas

--- src/common/ftp/ftp.service.ts
+++ src/common/ftp/ftp.service.ts
@@ -6,21 +6,23 @@ import { Readable, Writable } from 'stream';
 @Injectable()
 export class FtpService {
   private readonly logger = new Logger('FtpService');
-  private client: ftp.Client;
+  // private client: ftp.Client;
 
-  constructor() {
-    this.client = new ftp.Client();
-  }
+  // constructor() {
+  //   this.client = new ftp.Client();
+  // }
 
-  async connectToFtp() {
+  async connectToFtp(): Promise<ftp.Client> {
     try {
-      await this.client.access({
+      const client = new ftp.Client();
+      await client.access({
         host: envsFtp.ftpHost,
         user: envsFtp.ftpUsername,
         password: envsFtp.ftpPassword,
         secure: envsFtp.ftpSsl,
       });
       this.logger.log('Connected to FTP server successfully');
+      return client;
     } catch (error) {
       this.logger.error('Failed to connect to FTP server:', error);
       throw new Error('Failed to connect to FTP server');
@@ -28,22 +30,26 @@ export class FtpService {
   }
 
   async uploadFile(document: Buffer, initialPath: string, path: string) {
+    const client = await this.connectToFtp();
     try {
       const verifyPath = `${envsFtp.ftpRoot}${initialPath}`;
       const remotePath = `${envsFtp.ftpRoot}${path}`;
 
       const buffer = Buffer.from(document.buffer);
       const documentStream = Readable.from(buffer);
-      await this.client.ensureDir(verifyPath);
-      await this.client.uploadFrom(documentStream, remotePath);
+      await client.ensureDir(verifyPath);
+      await client.uploadFrom(documentStream, remotePath);
       this.logger.log('Uploaded file successfully');
     } catch (error) {
       this.logger.error('Failed to upload file:', error);
       throw new Error('Failed to upload file');
+    } finally {
+      this.onDestroy(client);
     }
   }
 
   async downloadFile(path: string) {
+    const client = await this.connectToFtp();
     try {
       const remoteFilePath = `${envsFtp.ftpRoot}${path}`;
       const chunks: Buffer[] = [];
@@ -53,28 +59,33 @@ export class FtpService {
           callback();
         },
       });
-      await this.client.downloadTo(writableStream, remoteFilePath);
+      await client.downloadTo(writableStream, remoteFilePath);
       this.logger.log('Downloaded file successfully');
       return Buffer.concat(chunks);
     } catch (error) {
       this.logger.error('Failed to download file:', error);
       throw new Error('Failed to download file:');
+    } finally {
+      this.onDestroy(client);
     }
   }
 
   async listFiles(path: string) {
+    const client = await this.connectToFtp();
     try {
       const remotePath = `${envsFtp.ftpRoot}${path}`;
-      const files = await this.client.list(remotePath);
+      const files = await client.list(remotePath);
       return files;
     } catch (error) {
       this.logger.error('Failed to list files:', error);
       throw new Error('Failed to list files');
+    } finally {
+      this.onDestroy(client);
     }
   }
 
-  async onDestroy() {
-    await this.client.close();
+  async onDestroy(client: ftp.Client) {
+    client.close();
     this.logger.log('FTP connection closed');
   }
 }
